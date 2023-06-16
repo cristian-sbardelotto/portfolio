c970c01035d932ac8525e1f5c08476978ed3cdab
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-06-16 14:20:57 -0300

    ♻️ refactor(skills): changed the way to render assets

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,26 +1,69 @@
 'use client';
 
+import { useState } from 'react';
+
 import { SkillsList } from '@/components/SkillsList';
 import { skills } from '@/data/skillsMock';
-
 import * as S from './styles';
 
+type SelectedAreaProps = 'frontend' | 'backend' | 'complementary';
+
+function renderAreaSkills(area: SelectedAreaProps) {
+  if (area === 'frontend') {
+    return skills.frontend.map((skill, index) => (
+      <SkillsList
+        key={index}
+        image={skill.image}
+        name={skill.name}
+      />
+    ));
+  }
+
+  if (area === 'backend') {
+    return skills.backend.map((skill, index) => (
+      <SkillsList
+        key={index}
+        image={skill.image}
+        name={skill.name}
+      />
+    ));
+  }
+
+  return skills.complementary.map((skill, index) => (
+    <SkillsList
+      key={index}
+      image={skill.image}
+      name={skill.name}
+    />
+  ));
+}
+
 export default function Skills() {
+  const [selectedArea, setSelectedArea] =
+    useState<SelectedAreaProps>('frontend');
+
   return (
     <S.Container>
       <S.StyledTitle>
         Technologies <span>&</span> Skills
       </S.StyledTitle>
 
-      <S.Skills>
-        {skills.map((skill, index) => (
-          <SkillsList
-            key={index}
-            image={skill.image}
-            name={skill.name}
-          />
-        ))}
-      </S.Skills>
+      <section>
+        <div
+          className='header'
+          style={{ display: 'flex', gap: '2rem' }}
+        >
+          <span onClick={() => setSelectedArea('frontend')}>Frontend</span>
+          <span onClick={() => setSelectedArea('backend')}>Backend</span>
+          <span onClick={() => setSelectedArea('complementary')}>
+            Complementary
+          </span>
+        </div>
+
+        <div className='body'>
+          <S.Skills>{renderAreaSkills(selectedArea)}</S.Skills>
+        </div>
+      </section>
     </S.Container>
   );
 }
