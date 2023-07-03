c0a72415ad4296011f2177e4d38ece026932d045
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-07-03 11:53:49 -0300

    ♻️ refactor(layout): created useTheme hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import { useState } from 'react';
-
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 
 import { GlobalStyle } from '@/styles/GlobalStyle';
-import dark from '@/styles/themes/dark';
-import light from '@/styles/themes/light';
-import { DefaultTheme, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
+import { useTheme } from '@/hooks/useTheme';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 
 const jakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] });
@@ -19,11 +16,7 @@ type LayoutProps = {
 };
 
 export default function RootLayout({ children }: LayoutProps) {
-  const [theme, setTheme] = useState<DefaultTheme>(dark);
-
-  function toggleTheme() {
-    setTheme(theme.title === 'light' ? dark : light);
-  }
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <ThemeProvider theme={theme}>
