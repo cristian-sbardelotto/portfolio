f71b32a1edd906cd324361d62feb5bb0b14bd430
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-03-13 15:17:10 -0300

    refactor: change projects data

--- src/data/projectsMock.ts
+++ src/data/projectsMock.ts
@@ -2,64 +2,38 @@ import { ProjectProps } from '@/types';
 
 import { v4 as randomUUID } from 'uuid';
 
-import imagineshop from '/public/images/imagineshop.png';
+import portfolio from '/public/images/portfolio.png';
 import memeCreator from '/public/images/meme-creator.png';
-import nlwSpacetime from '/public/images/nlw-spacetime.png';
-import qrCodeGenerator from '/public/images/qr-code-generator.png';
 import studion from '/public/images/studion.png';
 import todoList from '/public/images/todo-list.png';
+import cristianOS from '/public/images/cristian-os.png';
+import devPolls from '/public/images/dev-polls.png';
 
 export const projects: ProjectProps[] = [
   {
     id: randomUUID(),
-    title: 'NLW - Spacetime',
-    image: nlwSpacetime,
-    description:
-      'Spacetime is a Fullstack project that simulates a social network, allowing the user to upload images and add some text to publish their own memories.',
-    technologies: [
-      'typescript',
-      'nextjs',
-      'tailwind',
-      'nodejs',
-      'prisma',
-      'fastify',
-    ],
-    githubUrl: 'https://github.com/cristian-sbardelotto/nlw-spacetime',
+    title: 'Cristian OS',
+    image: cristianOS,
+    description: 'A little more about me. Also looks like Windows.',
+    technologies: ['typescript', 'react', 'tailwind'],
+    githubUrl: 'https://github.com/cristian-sbardelotto/cristian-os',
   },
   {
     id: randomUUID(),
     title: 'Meme Creator',
     image: memeCreator,
     description:
-      'The project is a creator of memes. The user can use an image of his own machine and customize the font color and decoration. After that, the user can also download the image. The website has light/dark themes.',
-    technologies: ['typescript', 'react', 'styled-components', 'vercel'],
+      'Create some memes, using any image you want. After that, download the meme and send it to you friends.',
+    technologies: ['typescript', 'react', 'styled-components'],
     githubUrl: 'https://github.com/cristian-sbardelotto/meme-creator',
-    deployUrl: 'https://meme-creator-kappa.vercel.app',
   },
   {
     id: randomUUID(),
-    title: 'ImagineShop',
-    image: imagineshop,
-    description:
-      'The project simulates an e-commerce site, that can buy items from the store with a false authentication.',
-    technologies: [
-      'typescript',
-      'reactjs',
-      'nextjs',
-      'styled-components',
-      'vercel',
-    ],
-    githubUrl: 'https://github.com/cristian-sbardelotto/imagineshop',
-    deployUrl: 'https://imagineshop-frontend-topaz.vercel.app',
-  },
-  {
-    id: randomUUID(),
-    title: 'ImagineShop API',
-    image: imagineshop,
-    description:
-      'The project simulates an users login API, and does simple functions, like create, read, update and delete.',
-    technologies: ['javascript', 'nodejs', 'mongodb', 'mongoose', 'dotenv'],
-    githubUrl: 'https://github.com/cristian-sbardelotto/imagineshop-api',
+    title: 'Portfolio',
+    image: portfolio,
+    description: 'Wait. You are already here. Then, check out the source code.',
+    technologies: ['typescript', 'nextjs', 'styled-components'],
+    githubUrl: 'https://github.com/cristian-sbardelotto/portfolio',
   },
   {
     id: randomUUID(),
@@ -67,41 +41,8 @@ export const projects: ProjectProps[] = [
     image: todoList,
     description:
       'Simple to-do list that allows user to create, delete and mark tasks.',
-    technologies: [
-      'typescript',
-      'reactjs',
-      'styled-components',
-      'vite',
-      'vercel',
-    ],
+    technologies: ['typescript', 'react', 'styled-components'],
     githubUrl: 'https://github.com/cristian-sbardelotto/to-do-list',
-    deployUrl: 'https://to-do-list-henna-three.vercel.app',
-  },
-  {
-    id: randomUUID(),
-    title: 'QR Code Generator',
-    image: qrCodeGenerator,
-    description:
-      'A simple QR Code generator that allows the user to download the image of any link that was typed. Idea provided by Sujeito Programador.',
-    technologies: ['javascript', 'reactjs', 'qr-code', 'vite', 'vercel'],
-    githubUrl: 'https://github.com/cristian-sbardelotto/qr-code-generator',
-    deployUrl: 'https://qr-code-generator-pied.vercel.app/',
-  },
-  {
-    id: randomUUID(),
-    title: 'Studion',
-    image: studion,
-    description: 'Page simulating an event manager, provided by Imagine School',
-    technologies: [
-      'html',
-      'css',
-      'javascript',
-      'jquery',
-      'slick-slider',
-      'vercel',
-    ],
-    githubUrl: 'https://github.com/cristian-sbardelotto/studion',
-    deployUrl: 'https://studion-eight.vercel.app/',
   },
   {
     id: randomUUID(),
@@ -109,7 +50,16 @@ export const projects: ProjectProps[] = [
     image: studion,
     description:
       'Manage and view events effortlessly. Register, list, and interact with detailed information. Efficient and flexible.',
-    technologies: ['typescript', 'nodejs', 'fastify', 'prisma', 'tsx', 'jwt'],
+    technologies: ['typescript', 'nodejs', 'fastify', 'prisma'],
     githubUrl: 'https://github.com/cristian-sbardelotto/studion-backend',
+  },
+  {
+    id: randomUUID(),
+    title: 'Dev Polls',
+    image: devPolls,
+    description:
+      'Vote in any poll you choose, and view the voting progression in real time. Made in the NLW Expert.',
+    technologies: ['typescript', 'nodejs', 'fastify', 'prisma'],
+    githubUrl: 'https://github.com/cristian-sbardelotto/dev-polls',
   },
 ];
