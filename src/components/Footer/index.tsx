04a5dbe1919530b89d3310c0ce80acc1e68e6e19
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-03-31 15:08:39 -0300

    refactor: update footer link href

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,12 +6,7 @@ import * as S from './styles';
 export function Footer() {
   return (
     <S.Footer>
-      <Link
-        target='_blank'
-        href='https://github.com/cristian-sbardelotto/'
-      >
-        Cristian Sbardelotto
-      </Link>
+      <Link href='/'>Cristian Sbardelotto</Link>
 
       <SocialLinks />
     </S.Footer>
