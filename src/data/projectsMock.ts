a70fce2a75e5081406d85c7cfa7237f0559804f6
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-22 17:41:15 -0300

    feat: update projects mock

--- src/data/projectsMock.ts
+++ src/data/projectsMock.ts
@@ -2,11 +2,11 @@ import { ProjectProps } from '@/types';
 
 import { v4 as randomUUID } from 'uuid';
 
-import portfolio from '/public/assets/projects/portfolio.svg';
-import memeCreator from '/public/assets/projects/meme-creator.svg';
-import imagineshop from '/public/assets/projects/imagineshop.svg';
-import todoList from '/public/assets/projects/todo-list.svg';
 import cristianOS from '/public/assets/projects/cristian-os.svg';
+import quizzed from '/public/assets/projects/quizzed.svg';
+import portfolio from '/public/assets/projects/portfolio.svg';
+import todoList from '/public/assets/projects/todo-list.svg';
+import memeCreator from '/public/assets/projects/meme-creator.svg';
 import qrCodeGenerator from '/public/assets/projects/qr-code-generator.svg';
 
 export const projects: ProjectProps[] = [
@@ -15,8 +15,17 @@ export const projects: ProjectProps[] = [
     title: 'Cristian OS',
     image: cristianOS,
     description: 'A little more about me. Also looks like Windows.',
-    technologies: ['typescript', 'react', 'tailwind'],
+    technologies: ['typescript', 'react', 'tailwind-css'],
     githubUrl: 'https://github.com/cristian-sbardelotto/cristian-os',
+  },
+  {
+    id: randomUUID(),
+    title: 'Quizzed!',
+    image: quizzed,
+    description:
+      'Test your knowledge and try our quiz to challenge yourself! Are you really a quiz wizard?',
+    technologies: ['typescript', 'react', 'tailwind-css'],
+    githubUrl: 'https://github.com/cristian-sbardelotto/quizzed/',
   },
   {
     id: randomUUID(),
@@ -25,15 +34,6 @@ export const projects: ProjectProps[] = [
     description: 'Wait. You are already here. Then, check out the source code.',
     technologies: ['typescript', 'next.js', 'styled-components'],
     githubUrl: 'https://github.com/cristian-sbardelotto/portfolio',
-  },
-  {
-    id: randomUUID(),
-    title: 'Meme Creator',
-    image: memeCreator,
-    description:
-      'Create some memes, using any image you want. After that, download the meme and send it to you friends.',
-    technologies: ['typescript', 'react', 'styled-components'],
-    githubUrl: 'https://github.com/cristian-sbardelotto/meme-creator',
   },
   {
     id: randomUUID(),
@@ -46,12 +46,12 @@ export const projects: ProjectProps[] = [
   },
   {
     id: randomUUID(),
-    title: 'ImagineShop',
-    image: imagineshop,
+    title: 'Meme Creator',
+    image: memeCreator,
     description:
-      'The project simulates an e-commerce website, where you can buy items from the store with a false authentication.',
-    technologies: ['typescript', 'next.js', 'styled-components'],
-    githubUrl: 'https://github.com/cristian-sbardelotto/imagineshop/',
+      'Create some memes, using any image you want. After that, download the meme and send it to you friends.',
+    technologies: ['typescript', 'react', 'styled-components'],
+    githubUrl: 'https://github.com/cristian-sbardelotto/meme-creator',
   },
   {
     id: randomUUID(),
